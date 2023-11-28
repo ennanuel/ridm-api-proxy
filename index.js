620c6985cb89459f8a5182ed4cbadc62314392e4
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-11-28 19:48:25 +0100

    Refactored code to look cleaner

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
+
 require('dotenv').config();
 
 app.use(
@@ -10,29 +11,29 @@ app.use(
     })
 )
 
-app.get(':endpoint([\\/\\w\\.-]*)', (req, res) => {
-    let endpoint = req.params.endpoint.includes('musixmatch') ? process.env.MUSIXMATCH_URL : process.env.DEEZER_URL
-    endpoint += req.params.endpoint.replace(/^\/[a-z]+/i, '')
+app.get(':endpoint([\\/\\w\\.-]*)', async (req, res) => {
+    try {
+        const { endpoint } = req.params;
+        const apiURL = endpoint.includes('musixmatch') ? process.env.MUSIXMATCH_URL : process.env.DEEZER_URL;
+        const apiEndpoint = endpoint.replace(/^\/[a-z]+/i, '');
+        const fullAPIPath = `${apiURL}${apiEndpoint}`;
 
-    let params = {}
+        let params = {};
+        for(let [field, value] of Object.entries(req.query) ) {
+            params[field] = value;
+        }
 
-    for( let [field, value] of Object.entries(req.query) ) {
-        params[field] = value
+        if(endpoint.includes('musixmatch')) {
+            params['apikey'] = process.env.MUSIXMATCH_API_KEY;
+        }
+
+        const response = await axios.get(fullAPIPath, { params });
+        const result = response.data;
+        return res.status(200).json(result); 
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: error.message });
     }
-
-    if(req.params.endpoint.includes('musixmatch')) {
-        params['apikey'] = process.env.MUSIXMATCH_API_KEY
-    }
-
-    axios.get(endpoint, {
-        params
-    })
-        .then(
-            response => res.json(response.data)
-        )
-        .catch( error => {
-            res.json(error)
-        })
 })
 
 app.listen(process.env.PORT || 5000, () => {
