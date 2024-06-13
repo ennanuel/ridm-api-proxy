e00ea53c80eb568a974646b867dd699aca7c9310
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-06-13 18:09:04 +0100

    Used 'PUT' method for 'deezer' axios fetch request

--- index.js
+++ index.js
@@ -15,21 +15,12 @@ app.use(
     })
 )
 
-function getParams(query) {
-    let params = {};
-    for (let [field, value] of Object.entries(query)) {
-        params[field] = value
-    }
-    return params;
-}
-
 app.get('/musixmatch/:endpoint([\\/\\w\\.-]*)', async function (req, res) { 
     try {
         const { endpoint } = req.params;
         const FULL_URL = `${process.env.MUSIXMATCH_URL}/${endpoint}`;
 
-        let params = getParams(req.query);
-        params['apikey'] = process.env.MUSIXMATCH_API_KEY;
+        let params = { ...req.query, apikey: process.env.MUSIXMATCH_API_KEY };
 
         const response = await axios.get(FULL_URL, { params, headers: HEADERS });
         const result = response.data;
@@ -45,9 +36,9 @@ app.get('/deezer/:endpoint([\\/\\w\\.-]*)', async function (req, res) {
         const { endpoint } = req.params;
         const FULL_URL = `${process.env.DEEZER_URL}/${endpoint}`;
 
-        let params = getParams(req.query);
+        let params = req.query;
 
-        const response = await axios.get(FULL_URL, { params, headers: HEADERS });
+        const response = await axios.put(FULL_URL, { params, headers: HEADERS });
         const result = response.data;
         return res.status(200).json(result); 
     } catch (error) {
@@ -56,31 +47,6 @@ app.get('/deezer/:endpoint([\\/\\w\\.-]*)', async function (req, res) {
     }
 });
 
-// app.get(':endpoint([\\/\\w\\.-]*)', async (req, res) => {
-//     try {
-//         const { endpoint } = req.params;
-//         const apiURL = endpoint.includes('musixmatch') ? process.env.MUSIXMATCH_URL : process.env.DEEZER_URL;
-//         const apiEndpoint = endpoint.replace(/^\/[a-z]+/i, '');
-//         const fullAPIPath = `${apiURL}${apiEndpoint}`;
-
-//         let params = {};
-//         for(let [field, value] of Object.entries(req.query) ) {
-//             params[field] = value;
-//         }
-
-//         if(endpoint.includes('musixmatch')) {
-//             params['apikey'] = process.env.MUSIXMATCH_API_KEY;
-//         }
-
-//         const response = await axios.get(fullAPIPath, { params });
-//         const result = response.data;
-//         return res.status(200).json(result); 
-//     } catch (error) {
-//         console.error(error);
-//         return res.status(500).json({ message: error.message });
-//     }
-// })
-
 app.listen(process.env.PORT || 5000, () => {
-    console.log('backend running')
+    console.log('proxy running...')
 })
